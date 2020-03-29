1b585a5f7e5d4a98449205041f953c6c3ddf0a65
Maria Soliman <development@mariasoliman.de>
2020-03-29 21:54:05 +0200

    start the sounds grid

--- public/player/playerUI.js
+++ public/player/playerUI.js
@@ -3,6 +3,30 @@
 
 jQuery(function ()
 {
+    $("#teamname").on('keyup blur', function(){
+        $('#step-name-button').prop('disabled', this.value.trim().length === 0);
+    });
+
+    jQuery('#step-name-button').on('click', function(e) {
+        jQuery('#step-name').addClass('hide');
+        jQuery('#step-sound').addClass('show');
+    });
+
+    jQuery('#step-sound-button').on('click', function(e) {
+        jQuery('#step-sound').addClass('hide');
+        jQuery('#step-play').addClass('show');
+    });
+
+    var sounds = {
+        awkestra: 'assets/sounds/Awkestra.mp3',
+        cena: 'assets/sounds/cena.mp3',
+        'crowd gasp': 'assets/sounds/Crowdgasp.mp3'
+    };
+
+    for (let key in sounds) {
+        jQuery('#sound-grid').append('<input type="radio" name="sound" id="' + key + '" value="' + key + '"><label for="' + key + '">' + key + '</label>')
+    }
+
     var socket = io();
     jQuery('#submit').on("click", function (e)
     {
