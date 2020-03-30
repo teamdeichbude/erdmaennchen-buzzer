f31ca2a67b73a7feefcd943193c9eaa9d005abd2
Maria Soliman <development@mariasoliman.de>
2020-03-30 17:55:34 +0200

    playing sounds in the grid yeeeaaaah

--- public/player/playerUI.js
+++ public/player/playerUI.js
@@ -17,27 +17,6 @@ jQuery(function ()
         jQuery('#step-play').addClass('show');
     });
 
-    var sounds = {
-        awkestra: 'assets/sounds/Awkestra.mp3',
-        cena: 'assets/sounds/cena.mp3',
-        gasp: 'assets/sounds/Crowdgasp.mp3',
-        error: 'assets/sounds/Error.mp3',
-        fart: 'assets/sounds/Flatulenz.mp3',
-        horn: 'assets/sounds/Horn.mp3',
-        hornstar: 'assets/sounds/Hornstar.mp3',
-        sheep: 'assets/sounds/Liesel.mp3',
-        goat: 'assets/sounds/Mähndy.mp3',
-        marimba: 'assets/sounds/Marimba.mp3',
-        oof: 'assets/sounds/Oof.mp3',
-        orchestra: 'assets/sounds/Orchestra.mp3',
-        plop: 'assets/sounds/plop.mp3',
-        ring: 'assets/sounds/Ring.mp3'
-    };
-
-    for (let key in sounds) {
-        jQuery('#sound-grid').append(' <input type="radio" name="sound" id="' + key + '" value="' + key + '"><label for="' + key + '">' + key + '</label>')
-    }
-
     jQuery('label').on('click', function(e) {
         jQuery('#step-sound-button').prop('disabled', false);
     })
---
+++ public/player/soundmanager.js
@@ -0,0 +1,77 @@
+jQuery(function () {
+    var sounds = {
+        awkestra: 'assets/sounds/Awkestra.mp3',
+        cena: 'assets/sounds/cena.mp3',
+        gasp: 'assets/sounds/Crowdgasp.mp3',
+        error: 'assets/sounds/Error.mp3',
+        fart: 'assets/sounds/Flatulenz.mp3',
+        horn: 'assets/sounds/Horn.mp3',
+        hornstar: 'assets/sounds/Hornstar.mp3',
+        sheep: 'assets/sounds/Liesel.mp3',
+        goat: 'assets/sounds/Mähndy.mp3',
+        marimba: 'assets/sounds/Marimba.mp3',
+        oof: 'assets/sounds/Oof.mp3',
+        orchestra: 'assets/sounds/Orchestra.mp3',
+        plop: 'assets/sounds/plop.mp3',
+        ring: 'assets/sounds/Ring.mp3'
+    };
+
+    for (let key in sounds) {
+        jQuery('#sound-grid').append(' <input type="radio" name="sound" id="' + key + '" value="' + key + '">'
+        +'<label for="' + key + '">' + key + '</label>'
+        +'<audio id="' + key+'audio' + '" src="../' + sounds[key] + '" type="audio/mpeg"></audio>');
+    }
+
+    var audiocontextplease = window.AudioContext || window.webkitAudioContext // Safari and old versions of Chrome
+                            || false;
+
+    if (audiocontextplease) {
+        const audioContext = new audiocontextplease;
+
+        // get the audio elements
+        const audioElements = jQuery('audio');
+
+        //console.log(audioElements);
+
+        // pass it into the audio context
+        for (let key in audioElements) {
+            //console.log(audioElements[key]);
+            if (audioElements[key] instanceof HTMLMediaElement) {
+                var mediaElement = audioContext.createMediaElementSource(audioElements[key]);
+                mediaElement.connect(audioContext.destination);
+            }
+        }
+
+        // select our play button
+        const playButton = document.querySelector('#playpause');
+
+        
+        jQuery('label').on('click', function(e) {
+            if (audioContext.state === 'suspended') {
+                audioContext.resume();
+            }
+            var audioId = '#' + jQuery(this).attr('for') + 'audio';
+            var audioElement = jQuery(this).siblings(audioId)[0];
+            audioElement.load();
+            audioElement.play();
+        });
+
+        /* playButton.addEventListener('click', function() {
+
+            // check if context is in suspended state (autoplay policy)
+            if (audioContext.state === 'suspended') {
+                audioContext.resume();
+            }
+
+            // play or pause track depending on state
+            if (this.dataset.playing === 'false') {
+                audioElement.play();
+                this.dataset.playing = 'true';
+            } else if (this.dataset.playing === 'true') {
+                audioElement.pause();
+                this.dataset.playing = 'false';
+            }
+
+        }, false); */
+    }
+});
