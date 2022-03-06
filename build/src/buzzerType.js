9b03c8e6e062e0fb6923060d8234a71b39ef7745
Luca <deichbu@lucaelsen.de>
2022-03-06 22:59:57 +0100

    delete build

--- build/src/buzzerType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BuzzerType = void 0;
-var BuzzerType;
-(function (BuzzerType) {
-    BuzzerType["text"] = "text";
-    BuzzerType["buzzer"] = "buzzer";
-})(BuzzerType = exports.BuzzerType || (exports.BuzzerType = {}));
-//# sourceMappingURL=buzzerType.js.map
